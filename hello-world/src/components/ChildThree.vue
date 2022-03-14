9b515d782c741106c23b2cb235a8893bdfa5ea05
sunxiaojia <sunxiaojia@bonree.com>
2022-03-14 18:07:27 +0800

    emit发送方式

--- hello-world/src/components/ChildThree.vue
+++ hello-world/src/components/ChildThree.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
+    <hr />
     {{ toCity }}
     <br />
-    <input placeholder="请输入想要去的城市" />
-    <button @click="$emit('changeCity', '湖北')" />
+    <input ref="city" placeholder="请输入想要去的城市" />
+    <button @click="$emit('changeCity', '湖北')">发送</button>
+    <br />
+    <button @click="$emit('changeCity', $refs.city.value)">发送</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'childThree',
+
   props: ['toCity']
 }
 </script>
